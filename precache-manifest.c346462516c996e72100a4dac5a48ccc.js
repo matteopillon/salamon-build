7bac82e4b1da8e65e1739aa366ea0f6f6d68a5bb
Matteo Pillon <matteo.pillon@metide.com>
2020-09-12 00:40:07 +0200

    First build

---
+++ precache-manifest.c346462516c996e72100a4dac5a48ccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceec847cf57f0b503426b8625f86002c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b59e9b999bf08b81c7c",
+    "url": "/static/css/2.93ba255e.chunk.css"
+  },
+  {
+    "revision": "4c0654fe1b7c7bf87fbd",
+    "url": "/static/css/main.9de83e2a.chunk.css"
+  },
+  {
+    "revision": "2b59e9b999bf08b81c7c",
+    "url": "/static/js/2.a70f8532.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.a70f8532.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c0654fe1b7c7bf87fbd",
+    "url": "/static/js/main.c330defe.chunk.js"
+  },
+  {
+    "revision": "1eb5a0a1848416ccb4e9",
+    "url": "/static/js/runtime-main.39c8042d.js"
+  }
+]);
